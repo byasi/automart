e58c01f34a036fb28b02bdca71595d8a1281b4c5
byasi <solomonbyasi@gmail.com>
2020-04-14 18:33:14 +0300

    first commit

---
+++ js/main.js
@@ -0,0 +1,52 @@
+// Design a product called AutoMart
+// Automart is a platform where users can post cars which they want to sell
+// And then other users can search for different cars and they can buy them
+
+// User Stories
+
+// Design an HTML template where users can do the following
+// 1. User can register on the platform
+// 2. User can login
+// 3. Users can view all the car posts
+// 4. Users can view a single car
+// 5. Users can Buy a car
+// 6. Users can post Car Ads
+// 7. Users can delete car Adverts
+// 8. Users can flag their cars as sold
+
+// Backend
+// Design APIs using Nodejs where users can do the following
+// 1. Users can register
+// const user = {
+//     name: "",
+//     address: "",
+//     phone: "",
+//     email: "",
+//     password: ""
+// }
+
+// 2. User can login
+// const loginData = {
+//     email: "",
+//     password: ""
+// }
+
+// 3. User can add a Car advert
+// const carAdvert = {
+//     name: "",
+//     color: "",
+//     model: "",
+//     year: "",
+//     price: "",
+//     photos: ""
+// }
+
+// 4. Users can view all car adverts
+
+// 5. Users can view a  single car
+
+// 6. Users can buy a car
+
+// 7. Users can flag their cars as sold
+
+// 8. Users can delete car Adverts
---
+++ readme.md
@@ -0,0 +1,8 @@
+# Automart Website
+This is my First project on Github as I start my programming journey.
+
+## Description
+Automart is a website built with Javascript and HTML5.
+It is a platform where people can sell and buy used and 
+new cars to each other
+
